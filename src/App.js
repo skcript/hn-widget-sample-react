584a20ab2e50644fe8a39dcb7d78de420018d1a0
Varun Raj <varun@skcript.com>
2021-11-12 23:29:17 +0530

    Initial Commit 🚀

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    window.HNWidget.init({
+      token: "<TOKEN HERE>",
+      selector: "#WidgetSelector", // Optional: Widget will open up when this element is clicked
+      placement: "left", // Optional: Placement of the element when selector is used
+      showOnlySubmission: false, // Optional: Toggle for showing only submission form or post list
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button id="WidgetSelector">Click Me</button>
     </div>
   );
 }
